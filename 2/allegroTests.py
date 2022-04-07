54878ae7bf48cdf3118d4c18642140c3d503a952
JKwidzinski <jakub.kwidzinski@gmail.com>
2022-04-07 16:14:05 +0200

    [Task2] Added logging.

--- 2/allegroTests.py
+++ 2/allegroTests.py
@@ -11,14 +11,19 @@ from selenium.webdriver.support.select import Select
 import time
 import logging
 
+logging.basicConfig(filename='allegroTestsLog.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+
 def ChromeSearchTshirtTest():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    logging.info('Going to allegro.pl')
     driver.get("https://allegro.pl/")   
     
-    try:        
+    try:
+        logging.info('Accepting cookies')        
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//button[@data-role="accept-consent"]')))
         element.click()
         
+        logging.info('Searching for koszulka')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//input[@type="text"]')))
         element.clear()
         element.send_keys("koszulka")
@@ -29,22 +34,28 @@ def ChromeSearchTshirtTest():
         driver.quit()
     except:
         driver.quit()
+        logging.exception('Exception occured')
 
 def ChromeChangeLanguageTest():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    logging.info('Going to allegro.pl')
     driver.get("https://allegro.pl/")   
     
-    try:        
+    try:
+        logging.info('Accepting cookies')        
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//button[@data-role="accept-consent"]')))
         element.click()
         
+        logging.info('Editing localization options')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//a[@data-analytics-click-label="ShippingCountryFlag"]')))
         element.click()
         
+        logging.info('Switching language to EN')
         select_element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'language-select')))
         select_object = Select(select_element)
         select_object.select_by_value("en-US")
         
+        logging.info('Confirming changes')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//button[text()="Zmień"]')))
         element.click()
         
@@ -53,15 +64,19 @@ def ChromeChangeLanguageTest():
         driver.quit()
     except:
         driver.quit()
+        logging.exception('Exception occured')
         
 def FirefoxSearchTshirtTest():
     driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    logging.info('Going to allegro.pl')
     driver.get("https://allegro.pl/")   
     
-    try:        
+    try:
+        logging.info('Accepting cookies')       
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//button[@data-role="accept-consent"]')))
         element.click()
         
+        logging.info('Searching for koszulka')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//input[@type="text"]')))
         element.clear()
         element.send_keys("koszulka")
@@ -72,22 +87,28 @@ def FirefoxSearchTshirtTest():
         driver.quit()
     except:
         driver.quit()
+        logging.exception('Exception occured')
 
 def FirefoxChangeLanguageTest():
     driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    logging.info('Going to allegro.pl')
     driver.get("https://allegro.pl/")   
     
-    try:        
+    try:
+        logging.info('Accepting cookies')        
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//button[@data-role="accept-consent"]')))
         element.click()
         
+        logging.info('Editing localization options')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//a[@data-analytics-click-label="ShippingCountryFlag"]')))
         element.click()
         
+        logging.info('Switching language to EN')
         select_element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'language-select')))
         select_object = Select(select_element)
         select_object.select_by_value("en-US")
         
+        logging.info('Confirming changes')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//button[text()="Zmień"]')))
         element.click()
         
@@ -96,6 +117,7 @@ def FirefoxChangeLanguageTest():
         driver.quit()
     except:
         driver.quit()
+        logging.exception('Exception occured')
 
 
 def main():
--- 2/tutorialsninjaTests.py
+++ 2/tutorialsninjaTests.py
@@ -13,42 +13,53 @@ import logging
 #Email needs to be changed in order to register account(acc with this email exists)
 def ChromeRegisterTest():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    logging.info('Going to tutorialsninja.com/demo')
     driver.get("http://tutorialsninja.com/demo/")   
     
-    try:        
+    try:
+        logging.info('Opening dropdown')        
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "a.dropdown-toggle")))
         element.click()
         
+        logging.info('Clicking Register')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.LINK_TEXT, "Register")))
         element.click()
         
+        logging.info('Inputting first name')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "input-firstname")))
         element.clear()
         element.send_keys("asd123")
         
+        logging.info('Inputting lastname')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "input-lastname")))
         element.clear()
         element.send_keys("asd123")
         
+        logging.info('Inputting email')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "input-email")))
         element.clear()
         element.send_keys("asd123@wp.pl")
         
+        logging.info('Inputting telephone number')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "input-telephone")))
         element.clear()
         element.send_keys("123123123")
         
+        logging.info('Inputting password')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "input-password")))
         element.clear()
         element.send_keys("asd123")
         
+        logging.info('Inputting confirm password')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "input-confirm")))
         element.clear()
         element.send_keys("asd123")
         
+        logging.info('Agreeing to terms')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.NAME, "agree")))
         element.click()
         
+        logging.info('Confirming registration')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//input[@type='submit']")))
         element.click()
         
@@ -57,26 +68,33 @@ def ChromeRegisterTest():
         driver.quit()
     except:
         driver.quit()
+        logging.exception('Exception occured')
         
 def ChromeLoginTest():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    logging.info('Going to tutorialsninja.com/demo')
     driver.get("http://tutorialsninja.com/demo/")   
     
-    try:        
+    try:
+        logging.info('Opening dropdown')         
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "a.dropdown-toggle")))
         element.click()
         
+        logging.info('Clicking Login')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.LINK_TEXT, "Login")))
         element.click()
         
+        logging.info('Inputting email')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "input-email")))
         element.clear()
         element.send_keys("asd123@wp.pl")
         
+        logging.info('Inputting password')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "input-password")))
         element.clear()
         element.send_keys("asd123")
         
+        logging.info('Confirming login process')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//input[@type='submit']")))
         element.click()
         
@@ -85,46 +103,58 @@ def ChromeLoginTest():
         driver.quit()
     except:
         driver.quit()
+        logging.exception('Exception occured')
 
 #Email needs to be changed in order to register account(acc with this email exists)        
 def FirefoxRegisterTest():
     driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    logging.info('Going to tutorialsninja.com/demo')
     driver.get("http://tutorialsninja.com/demo/")
     
-    try:        
+    try:
+        logging.info('Opening dropdown')        
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "a.dropdown-toggle")))
         element.click()
         
+        logging.info('Clicking Register')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.LINK_TEXT, "Register")))
         element.click()
         
+        logging.info('Inputting first name')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "input-firstname")))
         element.clear()
         element.send_keys("123asd")
         
+        logging.info('Inputting lastname')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "input-lastname")))
         element.clear()
         element.send_keys("123asd")
         
+        logging.info('Inputting email')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "input-email")))
         element.clear()
         element.send_keys("123asd@wp.pl")
         
+        logging.info('Inputting telephone number')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "input-telephone")))
         element.clear()
         element.send_keys("321321321")
         
+        logging.info('Inputting password')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "input-password")))
         element.clear()
         element.send_keys("123asd")
         
+        logging.info('Inputting confirm password')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "input-confirm")))
         element.clear()
         element.send_keys("123asd")
         
+        logging.info('Agreeing to terms')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.NAME, "agree")))
         element.click()
         
+        logging.info('Confirming registration')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//input[@type='submit']")))
         element.click()
         
@@ -132,27 +162,34 @@ def FirefoxRegisterTest():
         time.sleep(20)
         driver.quit()
     except:
-        driver.quit()        
+        driver.quit()
+        logging.exception('Exception occured')       
         
 def FirefoxLoginTest():
     driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    logging.info('Going to tutorialsninja.com/demo')
     driver.get("http://tutorialsninja.com/demo/")   
     
-    try:        
+    try:
+        logging.info('Opening dropdown')        
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "a.dropdown-toggle")))
         element.click()
         
+        logging.info('Clicking Login')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.LINK_TEXT, "Login")))
         element.click()
         
+        logging.info('Inputting Email')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "input-email")))
         element.clear()
         element.send_keys("asd123@wp.pl")
         
+        logging.info('Inputting password')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "input-password")))
         element.clear()
         element.send_keys("asd123")
         
+        logging.info('Confirming login process')
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//input[@type='submit']")))
         element.click()
         
@@ -161,6 +198,7 @@ def FirefoxLoginTest():
         driver.quit()
     except:
         driver.quit()
+        logging.exception('Exception occured')
 
 def main():
     ChromeRegisterTest()
